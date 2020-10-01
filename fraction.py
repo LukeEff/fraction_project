4e2846ec47baa031cfae72e6406e48166aa89a00
lukeeff <luke4nandez@gmail.com>
2020-10-01 13:02:58 -0700

    Initial commit

---
+++ fraction.py
@@ -0,0 +1,41 @@
+"""
+    Class example
+
+"""
+
+
+class Fraction:
+    def __init__(self, num=0, den=1):
+        self.__num: int = num
+        self.__den: int = den
+
+    def __str__(self) -> str:
+        return f"{self.__num}/{self.__den}"
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    @property
+    def num(self):
+        return self.__num
+
+    @@property
+    def den(self):
+        return self.__den
+
+    def get_den(self):
+        return self.__den
+
+    def get_num(self):
+        return self.__num
+
+    def __mul__(self, f):
+        num = self.get_num() * f.get_num()
+        den = self.get_den() * f.get_den()
+        return Fraction(num, den)
+
+if __name__ == "__main__":
+    f1 = Fraction(2,3)
+    print(f1, f1.get_num(), f1.get_den())
+    f2 = Fraction(4,5)
+    print(f1 * f2)
---
+++ self_check_01.py
@@ -0,0 +1,74 @@
+"""
+    Class example
+
+"""
+from typing import List, Set
+import random
+import string
+
+
+class Self_Check_01:
+    def __init__(self, lst=['cat', 'dog', 'rabbit']):
+        self.lst = lst
+
+    def __str__(self) -> str:
+        return f"{self.lst} {self.letters}"
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    # Original
+    def to_letters(self) -> list:
+        letters: List[str] = []
+        for word in self.lst:
+            for lett in word:
+                letters.append(lett)
+        return letters
+
+    # Single letter, set, no duplicates
+    def to_letters_set(self) -> set:
+        letters = set()
+        for word in self.lst:
+            for lett in word:
+                letters.add(lett)
+        return letters
+
+    # Letter, list comprehension
+    def to_letters_comp(self) -> list:
+        letters = [letter for word in self.lst for letter in word]
+        return letters
+
+    # Single letter, list comprehension
+    def to_letters_comp(self) -> list:
+        letters = []
+        [letters.append(letter) for word in self.lst for letter in word if not letters.__contains__(letter)]
+        return letters
+
+    def shakespeare(self):
+
+        closest = ('', -1)
+        quote = "methinks it is like a weasel"
+        length = len(quote)
+        letters = string.ascii_lowercase + ' '
+
+        for i in range(10000000):
+            scrambled = self.getRandomShake(letters, length)
+            close = self.check_str_shake(quote, scrambled)
+            closest = close if close[1] > closest[1] else closest
+
+        return closest
+
+
+    def check_str_shake(self, quote, scrambled):
+        count = 0
+        for i in range(len(quote)):
+            if quote[i] == scrambled[i]:
+                count += 1
+        return scrambled, count
+
+    def getRandomShake(self, letters, length):
+        return ''.join(random.choice(letters) for i in range(length))
+
+if __name__ == "__main__":
+    f1 = Self_Check_01(['cat', 'dog', 'rabbit'])
+    print(f1.shakespeare())
